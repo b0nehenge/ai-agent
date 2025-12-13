d0d58595d0f149023cf7bc366c43dc3615bdad14
b0nehenge <metallica99mm@gmail.com>
2025-12-13 08:00:25 -0800

    made ai prompt more dynamic

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+import argparse
 import os
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+# Now we can access `args.user_prompt`
+
+
+print(args)
 api_key = os.environ.get("GEMINI_API_KEY")
 
 if api_key is None:
@@ -12,12 +22,12 @@ if api_key is None:
 
 client = genai.Client(api_key=api_key)
 
-res = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-)
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-if res.usage_metadata is not None:
+res = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+
+if res.usage_metadata is not None and args.verbose:
+    print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {res.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {res.usage_metadata.candidates_token_count}")
 
